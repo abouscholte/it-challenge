64d7c630d948f62a8d32e845097b244a495189d5
Alain Bouscholte <alain@alains-air.home>
2021-02-19 13:28:55 +0100

    Update build

--- frontend/build/static/js/14.d8d16869.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpfrontend=this.webpackJsonpfrontend||[]).push([[14],{1407:function(t,e,n){"use strict";n.r(e);n(0);var a=n(3),o=n(2);e.default=function(){return localStorage.clear(),Object(o.jsx)(a.a,{to:{pathname:"/account/inloggen",state:{alert:"U bent nu succesvol uitgelogd!"}}})}}}]);
-//# sourceMappingURL=14.d8d16869.chunk.js.map
