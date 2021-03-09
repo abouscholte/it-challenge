72cfa566183ca18fbbf4f4149c786040fed8bd97
Alain Bouscholte <alain@alains-air.home>
2021-03-09 11:45:56 +0100

    Added book filter

--- frontend/src/pages/admin/books.js
+++ frontend/src/pages/admin/books.js
@@ -16,6 +16,8 @@ export default function Books() {
   // set constants and such
   const fetchBooks = FetchBooks()
   const [books, setBooks] = useState([])
+  const [filteredBooks, setFilteredBooks] = useState([])
+  const [filter, setFilter] = useState('all')
   
   useEffect(() => {
     // set document title
@@ -23,28 +25,52 @@ export default function Books() {
 
     // fetch books
     setBooks(fetchBooks)
-  }, [fetchBooks, setBooks])
+    setFilteredBooks(fetchBooks.filter((item) => item.status == 0))
+  }, [fetchBooks, setBooks, setFilteredBooks])
+
+  // create book card
+  const BookCard = item => {
+    
+    const book = item.item
+
+    return (
+      <Card key={book.id} two>
+        <div>
+          <CardTitle>{book.title}</CardTitle>
+          <CardSubtitle>{book.author}</CardSubtitle>
+        </div>
+        <CardList>
+          <li>Uitgever: {book.publisher}</li>
+          <li>Uitgegeven: {book.year_published}</li>
+          <li>ISBN: {book.isbn}</li>
+          <li>Type: {book.type == 'papier' ? 'Papieren boek' : book.type == 'audio' ? 'Audioboek' : 'E-book'}</li>
+        </CardList>
+        <CardLink to={`/boeken/boek-${book.id}`}>Wijzig boek</CardLink>
+      </Card>
+    )
+  }
 
   return (
     <React.Fragment>
       <Navbar />
       <Page title="Administrator paneel" sidebarTitle="Beheer alle boeken" sidebarType="admin">
+
+        {/* top information */}
+        <h1>Beheer alle boeken</h1>
+        <p className="large">Om alleen nieuwe, nog niet goedgekeurde boeken te zien, gebruik de filter hieronder.</p>
+
+        {/* filter button */}
+        <div className="small" style={{ marginBottom: 30 }} onClick={() => setFilter(filter == 'all' ? 'new' : 'all')}>
+          {(filter == 'all') ? 'Laat alleen nieuwe boeken zien' : 'Laat alle boeken zien'}
+        </div>
+        
+        {/* cards section */}
         <CardsSection>
-          {books.map(item => (
-            <Card key={item.id} two>
-              <div>
-                <CardTitle dangerouslySetInnerHTML={{ __html: item.title }} />
-                <CardSubtitle dangerouslySetInnerHTML={{ __html: item.author }} />
-              </div>
-              <CardList>
-                <li dangerouslySetInnerHTML={{ __html: `Uitgever: ${item.publisher}` }} />
-                <li>Uitgegeven: {item.year_published}</li>
-                <li>ISBN: {item.isbn}</li>
-                <li>Type: {item.type == 'papier' ? 'Papieren boek' : item.type == 'audio' ? 'Audioboek' : 'E-book'}</li>
-              </CardList>
-              <CardLink to={`/boeken/boek-${item.id}`}>Wijzig boek</CardLink>
-            </Card>
-          ))}
+          {
+            filter == 'all'  
+              ? books.map((item) => <BookCard key={item.id} item={item} />)
+              : filteredBooks.map((item) => <BookCard key={item.id} item={item} />)
+          }
         </CardsSection>
       </Page>
     </React.Fragment>
