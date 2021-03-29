dfd742335981fb8045ed8e26031cf5ac5da8d452
Alain Bouscholte <alain@alains-air.home>
2021-03-29 15:03:14 +0200

    GitHub Single Page App

---
+++ github/src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import App from './App';
+
+import './styles/main.scss';
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ github/src/modules/Top.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from '../styles/content.module.scss';
+import { LogoGithub } from 'react-ionicons';
+
+export default function Top() {
+  return (
+    <div className={styles.content}>
+      <h1 className={styles.title}>
+        Bekijk de code
+        <span className={styles.logo}>
+          <LogoGithub color={'#003162'} width={'30px'} height={'30px'} />
+        </span>
+      </h1>
+
+      <p className={styles.infoline}>
+        Alle code van de website voor de IT Challenge voor Notenboom Business School is te vinden op GitHub.
+      </p>
+
+      <a className={styles.button} href="https://github.com/abouscholte/it-challenge" target="_blank">
+        Ga naar GitHub
+      </a>
+    </div>
+  );
+}
