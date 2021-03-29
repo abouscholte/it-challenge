5a07b29016cea94ecebea2e754a40ad72681847f
Alain Bouscholte <alain@alains-air.home>
2021-03-29 23:55:44 +0200

    Add dark mode

--- github/src/modules/Footer.js
+++ github/src/modules/Footer.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import styles from '../styles/footer.module.scss';
 
 export default function Footer() {
   return (
-    <footer id="footer" className={styles.footer}>
-      <p className={styles.text}>&copy; Copyright 2020 - 2021</p>
+    <footer id="footer" className="footer">
+      <p className="text">&copy; Copyright 2020 - 2021</p>
     </footer>
   );
 }
