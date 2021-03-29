79f627e630239ac13a86bc7cd82b1550b2f64d27
Alain Bouscholte <alain@alains-air.home>
2021-03-29 23:53:32 +0200

    Add dark mode

--- github/src/modules/About.js
+++ github/src/modules/About.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import styles from '../styles/content.module.scss';
 
 export default function About() {
   return (
-    <section id="about" className={styles.content}>
-      <h1 className={styles.title}>Wat is GitHub?</h1>
-      <p className={`${styles.infoline} ${styles.no_margin}`}>GitHub is een plek voor programmeurs om hun code te delen en onderhouden. GitHub is gebaseerd op Git, een <i>version control</i> systeem. Hierdoor zou je altijd terug kunnen naar een eerdere versie van je code als er bijvoorbeeld een fout is gemaakt.</p>
+    <section id="about" className="content">
+      <h1 className="title">Wat is GitHub?</h1>
+      <p className="infoline no_margin">GitHub is een plek voor programmeurs om hun code te delen en onderhouden. GitHub is gebaseerd op Git, een <i>version control</i> systeem. Hierdoor zou je altijd terug kunnen naar een eerdere versie van je code als er bijvoorbeeld een fout is gemaakt.</p>
     </section>
   );
 }
--- github/src/modules/Header.js
+++ github/src/modules/Header.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import styles from '../styles/content.module.scss';
 import { LogoGithub } from 'react-ionicons';
 
 export default function Top() {
+  const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+  
   return (
-    <section id="header" className={styles.content}>
-      <h1 className={styles.title}>
+    <section id="header" className="content">
+      <h1 className="title">
         Bekijk de code
-        <span className={styles.logo}>
-          <LogoGithub color={'#003162'} width={'30px'} height={'30px'} />
+        <span className="logo">
+          <LogoGithub
+            color={isDarkMode ? '#2190ff' : '#003162'}
+            height={'30px'}
+            width={'30px'}
+          />
         </span>
       </h1>
 
-      <p className={styles.infoline}>
+      <p className="infoline">
         Alle code van de website voor de IT Challenge voor Notenboom Business School is te vinden op GitHub.
       </p>
 
-      <a className={styles.button} href="https://github.com/abouscholte/it-challenge" target="_blank" rel="noreferrer">
+      <a className="button" href="https://github.com/abouscholte/it-challenge" target="_blank" rel="noreferrer">
         Breng me naar GitHub
       </a>
-      <a className={`${styles.button} ${styles.button_outline}`} href="#about">Wat is GitHub?</a>
+      <a className="button button_outline" href="#about">Wat is GitHub?</a>
     </section>
   );
 }
