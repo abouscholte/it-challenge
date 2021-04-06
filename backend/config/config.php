0f37741c09542135afd112c38bd2430afba56cf9
Alain Bouscholte <alain@alain.home>
2021-04-06 20:58:08 +0200

    Removed files

--- backend/config/config.php
+++ backend/config/config.php
@@ -2,6 +2,6 @@
 
 class Config {
   // define variables
-  public $site_url = "https://informaticaserver.nl/notenboom_project";
+  public $site_url = "https://informaticaserver.nl/notenboom_project/site";
   public $jwt_secret = 'thisisverysecret';
 }
--- code/src/modules/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Footer() {
-  return (
-    <footer id="footer" className="footer">
-      <p className="text">&copy; Copyright 2020 - 2021</p>
-    </footer>
-  );
-}
